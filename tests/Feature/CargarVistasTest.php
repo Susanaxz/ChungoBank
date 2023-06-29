f9dd55cb84fe391b19362b81ed5a487481eaf548
Susanaxz <riberasusana@gmail.com>
2023-06-29 12:24:08 +0200

    feat: test con php unit

---
+++ tests/Feature/CargarVistasTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class CargarVistasTest extends TestCase
+{
+    /**
+     * Prueba la vista de la página de inicio
+     */
+
+    public function testCarga_Inicio(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200)->assertViewIs('gestion');
+    }
+
+    /**
+     * Prueba la vista de la página de alta de movimientos
+     */
+    public function testCarga_AltaMovimientos(): void
+    {
+        $response = $this->get('/alta-movimientos');
+
+        $response->assertStatus(200)->assertViewIs('alta-movimientos');
+    }
+
+    /**
+     * Prueba la vista de la página de alta de mantenimiento de puntos
+     */
+    public function testCarga_AltaMtoPuntos(): void
+    {
+        $response = $this->get('/alta-mto-puntos');
+
+        $response->assertStatus(200)->assertViewIs('alta-mto-puntos');
+    }
+
+    /**
+     * Prueba la vista de la página de alta de personas
+     */
+    public function testCarga_AltaPersonas(): void
+    {
+        $response = $this->get('/alta-personas');
+
+        $response->assertStatus(200)->assertViewIs('alta-personas');
+    }
+
+    /**
+     * Prueba la vista de la página de consulta de movimientos
+     */
+    public function testCarga_ConsultaMovimientos(): void
+    {
+        $response = $this->get('/consulta-movimientos');
+
+        $response->assertStatus(200)->assertViewIs('consulta-movimientos');
+    }
+
+    /**
+     * Prueba la vista de la página de detalle de movimientos
+     */
+    public function testCarga_DetalleMovimiento(): void
+    {
+        $response = $this->get('/detalle-movimiento');
+
+        $response->assertStatus(200)->assertViewIs('detalle-movimiento');
+    }
+
+    /**
+     * Prueba la vista de la página de gestión
+     */
+    public function testCarga_Gestion(): void
+    {
+        $response = $this->get('/gestion');
+
+        $response->assertStatus(200)->assertViewIs('gestion');
+    }
+
+    /**
+     */
+    public function __construct() {
+    }
+}
+
