23327d5ebeb4a6ff79ffbbe65957ad03bb79eb7e
Susanaxz <riberasusana@gmail.com>
2023-07-10 13:17:25 +0200

    feat: creacion Moviemientos Model

---
+++ app/Models/Movimientos.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Movimientos extends Model
+{
+    use HasFactory;
+    
+    public static function movimientos($datos)
+    {
+        return Movimientos::create([
+            'fecha' => $datos['fecha'],
+            'operacion' => $datos['operacion'],
+            'concepto' => $datos['concepto'],
+            'puntos' => $datos['puntos'],
+            'saldomov' => $datos['saldomov'],
+            'tarjeta' => $datos['tarjeta'],
+            'localizador' => $datos['localizador'],
+            'comercio' => $datos['comercio'],
+            'comentarios' => $datos['comentarios'],
+            'cuenta_id' => $datos['cuenta_id']
+        ]);
+    }
+
+    protected $fillable = [
+        'fecha',
+        'operacion',
+        'concepto',
+        'puntos',
+        'saldomov',
+        'tarjeta',
+        'localizador',
+        'comercio',
+        'comentarios',
+        'cuenta_id'
+    ];
+// Relación de uno a muchos inversa (muchos a uno) de movimientos a cuentas
+//     public function cuenta()
+//     {
+//         return $this->belongsTo(Cuentas::class);
+//     }
+}
